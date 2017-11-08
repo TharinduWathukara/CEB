660490d596739f70289b568f67d8d851a8a81f69
TharinduWathukara <wtgwathukara@gmail.com>
2017-11-08 16:49:42 +0530

    2 forms ok

--- home.php
+++ home.php
@@ -66,10 +66,10 @@ include_once 'header.php';
                             </a>
                             <div class="collapse" id="toggleDemo1" style="height: 0px;">
                                 <ul class="nav nav-list">
-                                    <li><a href="#">Add New Employee</a></li>
-                                    <li><a href="#">Edit Employee Details</a></li>
-                                    <li><a href="#">Add new Customer</a></li>
-                                    <li><a href="#">Edit Customer Details</a></li>
+                                    <li id="addEmployeeMenu" onclick="document.getElementById('addEmployee').style.display='block'" style="cursor: pointer;"><a>Add New Employee</a></li>
+                                    <li id="editEmployeeDetailsMenu" style="cursor: pointer;"><a>Edit Employee Details</a></li>
+                                    <li id="addCustomerMenu" onclick="document.getElementById('addCustomer').style.display='block'" style="cursor: pointer;"><a>Add new Customer</a></li>
+                                    <li id="editCustomerDetailsMenu" style="cursor: pointer;"><a>Edit Customer Details</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -109,6 +109,131 @@ include_once 'header.php';
 <!--            what you want to add.. add here-->
 
 
+<!--            add new employee-->
+
+            <div id="addEmployee" style="margin: 0 auto; width: 58%; border: 2px solid black; padding: 30px; display: none">
+                <div class="form-area">
+                    <form role="form">
+                        <br style="clear:both">
+                        <h3 style="margin-bottom: 25px; text-align: center;">ADD NEW EMPLOYEE</h3>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Name" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" placeholder="Email" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="mobile" placeholder="Mobile Number" required>
+                        </div>
+                        <!--<div class="form-group">
+                            <select class="form-control" style="width: 55%;">
+                                <option selected>Choose Branch...</option>
+                            </select>
+                        </div>-->
+                        <div class="row">
+                            <div class="form-group" style="padding-left: 16px"><strong>Designation :</strong></div>
+                            <div class="form-check form-check-inline col-sm-4">
+                                <label>
+                                    <input onclick="document.getElementById('form_serviceArea').style.display='none'" class="form-check-input" type="radio" name="position" value="manager"> Manager
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline col-sm-4">
+                                <label>
+                                    <input onclick="document.getElementById('form_serviceArea').style.display='none'" class="form-check-input" type="radio" name="position" value="cachier"> Cashier
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline col-sm-4">
+                                <label>
+                                    <input onclick="document.getElementById('form_serviceArea').style.display='block'" class="form-check-input" type="radio" name="position" value="meterReader"> Meter Reader
+                                </label>
+                            </div>
+                        </div><br>
+                        <div class="form-group" id="form_serviceArea" style="display:none;">
+                            <input type="text" class="form-control" name="serviceArea" placeholder="Service Area" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password" placeholder="Temporary Password" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="cpassword" placeholder="Confirm Password" required>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <button type="submit" id="submit" name="submit" class="btn btn-primary" style="width: 90%">Add</button>
+                            </div>
+                            <div class="col-sm-6">
+                                <button type="button" style="width: 90%" onclick="document.getElementById('addEmployee').style.display='none'" class="btn btn-primary ">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+<!--            add new customer-->
+
+            <div id="addCustomer" style="margin: 0 auto; width: 58%; border: 2px solid black; padding: 30px; display: none;">
+                <div class="form-area">
+                    <form role="form">
+                        <br style="clear:both">
+                        <h3 style="margin-bottom: 25px; text-align: center;">ADD NEW CUSTOMER</h3>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Name" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="address" placeholder="Address" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" placeholder="Email" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="accountNumber" placeholder="Account Number" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="mobile" placeholder="Mobile Number" required>
+                        </div>
+                        <!--<div class="form-group">
+                            <select class="form-control" style="width: 55%;">
+                                <option selected>Choose Branch...</option>
+                            </select>
+                        </div>-->
+                        <div class="form-group">
+                            <select class="form-control" style="width: 55%;">
+                                <option selected>Choose Customer Type...</option>
+                                <option>House</option>
+                                <option>Government Organization</option>
+                                <option>Enterprises</option>
+                                <option>Religious Establishment</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password" placeholder="Temporary Password" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="cpassword" placeholder="Confirm Password" required>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <button type="submit" id="submit" name="submit" class="btn btn-primary" style="width: 90%">Add</button>
+                            </div>
+                            <div class="col-sm-6">
+                                <button type="button" style="width: 90%" onclick="document.getElementById('addCustomer').style.display='none'" class="btn btn-primary ">Cancel</button>
+                            </div>
+                        </div><br>
+                    </form>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
         </div>
     </div>
 </div>
@@ -120,5 +245,7 @@ include_once "footer.php";
 </body>
 <script type="text/javascript" src="other/javascript/jquery-3.2.1.min.js"></script>
 <script src="other/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="other/javascript/homeScript.js"></script>
+==
 <html>
 
---
+++ other/php/query.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tharindu
+ * Date: 11/8/17
+ * Time: 10:00 AM
+ */
